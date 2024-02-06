bd8dc335ff21da8b74480d7f580edef598f81c51
A7mooz <teaming04@gmail.com>
2024-02-06 20:20:05 +0300

    Patching dmenu

--- config.def.h
+++ config.def.h
@@ -4,13 +4,15 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"monospace:size=10"
+	"Hack:size=16",
+	"Hack Nerd:size=16",
+	"Font Awesome:size=16"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeNorm] = { "#bbbbbb", "#000000" },
+	[SchemeSel] = { "#eeeeee", "#5b0077" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
